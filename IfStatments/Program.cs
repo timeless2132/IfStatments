b6ffe1b62371d73e5ee0a49fae326b275366e270
Carter Duffy <carterduffy4@gmail.com>
2025-02-11 14:31:34 -0500

    Add project files.

---
+++ IfStatments/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IfStatments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
