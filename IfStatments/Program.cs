2db5b90281c00cf0c383c26b618a1a9d8a4ca6d9
Carter Duffy <carterduffy4@gmail.com>
2025-02-11 14:45:13 -0500

    Did first part

--- IfStatments/Program.cs
+++ IfStatments/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Part 1
+            
+            Boolean correctDir = false;
+            while (correctDir == false)
+            {
+                Console.WriteLine("Enter a direction, this will tell you if its N E S or W");
+                int input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                if (input <= 0 || input >= 361)
+                {
+                    Console.WriteLine("Please enter a valid number.");
+                    Console.WriteLine();
+                }
+                else if (input >= 315 || input <= 45)
+                {
+
+                    Console.WriteLine("North");
+                   
+                }
+            }
+       
+
+
         }
     }
 }
