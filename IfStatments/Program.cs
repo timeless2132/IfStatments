d2dc5a26d834a43bcfcddc692bdaa81fff27cfbc
Carter Duffy <carterduffy4@gmail.com>
2025-02-12 14:20:08 -0500

    Did Switches.

--- IfStatments/Program.cs
+++ IfStatments/Program.cs
@@ -86,8 +86,30 @@
             }
            
             Console.WriteLine("Your fee after that much time would be: $" + fee);
+            Console.WriteLine();
 
-            
+            //Part 3
+
+            int Number = 5;
+
+            switch (Number)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+            }
 
 
         }
